c5827f2a64c8727b96443ba90f08a4512c932c74
Alreadydo <33254518+Alreadydo@users.noreply.github.com>
2022-02-06 22:55:18 +0100

    SQ

---
+++ infoborden/ListenerStarter.java
@@ -0,0 +1,55 @@
+package infoborden;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.security.auth.Subject;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public  class ListenerStarter implements Runnable, ExceptionListener {
+	private String selector="";
+	private Infobord infobord;
+	private Berichten berichten;
+	
+	public ListenerStarter() {
+	}
+	
+	public ListenerStarter(String selector, Infobord infobord, Berichten berichten) {
+		this.selector=selector;
+		this.infobord=infobord;
+		this.berichten=berichten;
+	}
+
+	public void run() {
+        try {
+            ActiveMQConnectionFactory connectionFactory = 
+            		new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_BROKER_URL);
+//			TODO maak de connection aan
+	        Connection connection = connectionFactory.createConnection();
+	        connection.start();
+	    	connection.setExceptionListener(this);
+//			TODO maak de session aan
+          	Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//			TODO maak de destination aan
+	        Destination destination = session.createTopic("boardTopic");
+//			TODO maak de consumer aan
+	        MessageConsumer consumer = session.createConsumer(destination, selector);
+            System.out.println("Produce, wait, consume"+ selector);
+//			TODO maak de Listener aan
+			QueueListener queueListener = new QueueListener("consumer", infobord, berichten);
+	        consumer.setMessageListener(queueListener);
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void onException(JMSException ex) {
+        System.out.println("JMS Exception occured.  Shutting down client.");
+    }
+}
